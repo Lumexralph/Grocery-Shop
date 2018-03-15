062a3d9cdd0806f81ff9bc5da2e1589115decdf4
Ogundele Olumide <olumideralph@gmail.com>
2018-03-15 02:18:09 +0100

    Get data from API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,18 +8,31 @@
  * Controller of the mod2LabApp
  */
 angular.module('mod2LabApp')
-  .controller('MainCtrl', ['MainService', 
-  function (mainService) {
-    this.active = 0;
-    this.toggleOn = 1;
-    this.imageSrc = [
-      'images/a.jpg',
-      'images/b.jpg'
-    ];
+  .controller('MainCtrl', ['MainService',
+    function (mainService) {
+      this.active = 0;
+      this.toggleOn = 1;
+      this.imageSrc = [];
 
-    // handle
+      // handle
 
-    mainService.message().then(result => this.message= result.data.message);
-  
-  }
-]);
+      mainService.message().then(result => {
+        let dataLength = result.data.length - 1;
+        let subcategoryLength = result.data[dataLength].subcategories.length - 1;
+        let itemLength = result.data[dataLength].subcategories[subcategoryLength].items.length - 1;
+
+        let subcategoryIndex = Math.round(Math.random() * subcategoryLength);
+        let itemIndex = Math.round(Math.random() * itemLength);
+
+
+        // generate items for the carousel imageSrc array
+        for (let i = 0; i <= dataLength; i++) {
+          let item = result.data[i].subcategories[subcategoryIndex].items[itemIndex];
+
+          this.imageSrc.push(item);
+        }
+        console.log(this.imageSrc);
+      });
+
+    }
+  ]);
