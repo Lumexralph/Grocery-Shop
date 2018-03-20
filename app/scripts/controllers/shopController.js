2d1f0c8445492327f5fa243971d6d741c8bf668f
Ogundele Olumide <olumideralph@gmail.com>
2018-03-20 10:53:54 +0100

    Dynamically update the subcategory and items in it

--- app/scripts/controllers/shopController.js
+++ app/scripts/controllers/shopController.js
@@ -2,7 +2,13 @@ angular.module('mod2LabApp')
      .controller('ShopController', ['MainService', '$scope',
        function (mainService, $scope) {
         this.data = [];
-        
+        this.category = [];
+        this.subcategory = [];
+        this.itemsInstock = [];
+        this.checkItemInStock = inStock;
+        this.itemInSubcategory = [];
+        this.getCategory = getCategory;
+
 
         // get data
         mainService.message().then(result => {
@@ -12,6 +18,36 @@ angular.module('mod2LabApp')
           this.itemInSubcategory = this.data[0].subcategories[0].items;
           console.log('data from shpController', this.data);
         });
+
+        // get items in stock
+        function inStock(items) {
+          items.forEach(element => {
+            // empty the present in stock data
+            this.itemsInstock = [];
+            if (element.stock > 0) {
+              this.itemsInstock.push(element);
+            }
+          });                 
+        }
+
+        function getCategory(categoryIndex, subcategoryIndex) {
+          let presentData = this.data[categoryIndex];
+
+          let category = presentData.category;
+
+          let subcategory = presentData.subcategories[subcategoryIndex];
+
+          let subcategoryName = subcategory.name;
+
+          let presentSubcategoryItem = subcategory.items;
+
+          // update the categories per click
+          this.category = category;
+          this.subcategory = subcategoryName;
+          this.itemInSubcategory = presentSubcategoryItem;
+
+          console.log(category, subcategory)
+        }
        }
      ]);
 
