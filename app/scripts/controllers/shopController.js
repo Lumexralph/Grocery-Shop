0607784e148d591224c0a02203bb36d1e54b0242
Ogundele Olumide <olumideralph@gmail.com>
2018-03-21 14:30:21 +0100

    Add In-stock only functionality

--- app/scripts/controllers/shopController.js
+++ app/scripts/controllers/shopController.js
@@ -1,34 +1,44 @@
 angular.module('mod2LabApp')
      .controller('ShopController', ['MainService', '$scope',
        function (mainService, $scope) {
+        this.toggleOn = 0;
         this.data = [];
         this.category = [];
         this.subcategory = [];
         this.itemsInstock = [];
-        this.checkItemInStock = inStock;
+        this.noOfItemsInstock = 0;
+        // this.checkItemInStock = inStock;
         this.itemInSubcategory = [];
         this.getCategory = getCategory;
 
 
-        // get data
+        // get data to start with
         mainService.message().then(result => {
           this.data = result.data;
           this.category = this.data[0].category;
           this.subcategory = this.data[0].subcategories[0].name;
           this.itemInSubcategory = this.data[0].subcategories[0].items;
-          console.log('data from shpController', this.data);
+
+          // to start the count of items at start of program
+          this.noOfItemsInstock = countItem(this.itemInSubcategory).noOfItemsAvailable;
+
+          this.itemsInstock = countItem(this.itemInSubcategory).itemInStock;
+
+          console.log('On receiving data:', this.itemsInstock)          
+          
         });
 
-        // get items in stock
-        function inStock(items) {
-          items.forEach(element => {
-            // empty the present in stock data
-            this.itemsInstock = [];
-            if (element.stock > 0) {
-              this.itemsInstock.push(element);
-            }
-          });                 
-        }
+        
+        // // get items in stock
+        // function inStock(items) {
+        //   items.forEach(element => {
+        //     // empty the present in stock data
+        //     this.itemsInstock = [];
+        //     if (element.stock > 0) {
+        //       this.itemsInstock.push(element);
+        //     }
+        //   });                 
+        // }
 
         function getCategory(categoryIndex, subcategoryIndex) {
           let presentData = this.data[categoryIndex];
@@ -41,12 +51,41 @@ angular.module('mod2LabApp')
 
           let presentSubcategoryItem = subcategory.items;
 
+          let availableItems = countItem(presentSubcategoryItem);
+          
+
           // update the categories per click
+          
           this.category = category;
           this.subcategory = subcategoryName;
           this.itemInSubcategory = presentSubcategoryItem;
+          this.itemsInstock = availableItems.itemInStock;
+          this.noOfItemsInstock = availableItems.noOfItemsAvailable;
 
-          console.log(category, subcategory)
+          // reset instock toggle
+          this.toggleOn = 0;
+
+          
+        }
+
+
+        function countItem(items) {
+
+          let noOfItemsAvailable = 0;
+
+          let itemInStock = [];
+
+          itemInStock = items.filter(el => el.stock > 0 );
+
+          console.log('from countitem function: ',itemInStock);
+
+          noOfItemsAvailable  = itemInStock.length;
+
+          return {
+            itemInStock,
+            noOfItemsAvailable
+          };
+          
         }
        }
      ]);
