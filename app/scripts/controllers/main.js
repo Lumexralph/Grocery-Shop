523b5389f4e201ffbcd1d4137dfb450d6a3209ad
Ogundele Olumide <olumideralph@gmail.com>
2018-03-22 01:47:49 +0100

    Change carousel to one product per slide and how images are loaded

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,9 +14,9 @@ angular.module('mod2LabApp')
       this.active = 0;
       this.toggleOn = 1;
       this.imageSrc = [];
-      this.randomNumber = function() {
-        return Math.floor((Math.random() * this.imageSrc.length));
-      };
+      // this.randomNumber = function() {
+      //   return Math.floor((Math.random() * this.imageSrc.length));
+      // };
 
       mainService.message().then(result => {
         this.data = result.data;
@@ -24,18 +24,18 @@ angular.module('mod2LabApp')
         let data = this.data;
                
         // generate items for the carousel imageSrc array
-        for (let i = 0, j = 0; j < 9; j++, i++) {
+        for (let i = 0, j = 0; j < 4; j++, i++) {
 
           i = i < data.length ? i : 0;
           let subcategory = data[i].subcategories;
 
-          let items = subcategory[Math.round(Math.random() * (subcategory.length - 1))].items;
-          console.log(items);
+          let items = _.shuffle(subcategory)[0].items;
+
           // if items exist in the category
           if(items[0]) {
             // check the length of item
             let itemsLength = items.length;
-            let item = items[Math.round(Math.random() * (items.length - 1))];
+            let item = _.shuffle(items)[0];
 
             // add item for image carousel
             this.imageSrc.push(item);
