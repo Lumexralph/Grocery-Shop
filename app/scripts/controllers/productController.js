749a552320d09a825c5c9c85a5d011ec5ff7bcc9
Ogundele Olumide <olumideralph@gmail.com>
2018-03-23 06:01:51 +0100

    Add query to the product route and inject dependency

--- app/scripts/controllers/productController.js
+++ app/scripts/controllers/productController.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('mod2LabApp')
-    .controller('ProductController', ['MainService', function (mainService, $routeParams) {
+    .controller('ProductController', ['MainService', '$routeParams',
+    function (mainService, $routeParams) {
 
       this.params = $routeParams;
-      console.log(this.params);
+      console.log('param',this.params);
       
     }]);
