a4fc79e37c075cb064a09ce934fe72bbc1c0e4fb
Ogundele Olumide <olumideralph@gmail.com>
2018-03-23 13:05:27 +0100

    Create an array of all products

--- app/scripts/controllers/productController.js
+++ app/scripts/controllers/productController.js
@@ -5,6 +5,29 @@ angular.module('mod2LabApp')
     function (mainService, $routeParams) {
 
       this.params = $routeParams;
+      this.product;
+
       console.log('param',this.params);
+
+      let allProducts = [];
+
+      // inject data from API
+      mainService.message().then(result => {
+        let data = result.data;
+
+        // spread out all the items
+        data.forEach(el => {
+          el.subcategories.forEach(el => {
+              allProducts.push(...el.items);
+        
+          });
+        });
+
+        this.product = allProducts.find(el => el.name === this.params.name);
+
+        console.log('data from ProductController', this.product);
+      }
+    );
       
-    }]);
+    }
+  ]);
